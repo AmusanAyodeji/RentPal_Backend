c3a29aa276e04a7e04792a00bc93483b8cc42755
Ayodeji Amusan <ayobayonleamusan1@gmail.com>
2025-04-24 21:06:19 +0100

    configured CORS

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
 from routers.buildings import buildingrouter
 from routers.users import usersrouter
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://rent-pal.vercel.app/",
+]
 
 app.include_router(usersrouter,tags=["users"])
 app.include_router(buildingrouter,prefix="/buildings",tags=["buildings"])
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def home():
