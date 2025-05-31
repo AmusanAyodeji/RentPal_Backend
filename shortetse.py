30dc7615fc503fce096cc44168b59cc235ceaf08
Ayodeji Amusan <ayobayonleamusan1@gmail.com>
2025-05-31 14:32:23 +0100

    remvoed env

--- shortetse.py
+++ shortetse.py
@@ -1,9 +1,4 @@
-import smtplib
+import turtle
 
-sender = "rentpal92@gmail.com"
-password = "lrke dhqe zfna yydb"
-
-with smtplib.SMTP("smtp.gmail.com", 587) as server:
-    server.starttls()
-    server.login(sender, password)
-    print("Login successful!")
+turtle.showturtle()
+turtle.forward(200)
